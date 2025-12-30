676e223317ed7e56049e7bcce6abfcb7dd689a71
Michael Ampomah <162718355+ampomahmichael321@users.noreply.github.com>
2025-12-30 23:16:14 +0000

    Add files via upload

---
+++ list.js
@@ -0,0 +1,38 @@
+const main = document.querySelector(".main");
+let html = "";
+
+fetch("https://errand-app-backend.onrender.com/api/service_requests/")
+  .then((response) => response.json())
+  .then((items) => {
+    let svg_path = "";
+    items.forEach((item) => {
+      if (item.service_type == "delivery") {
+        svg_path =
+          "M280-160q-50 0-85-35t-35-85H60l18-80h113q17-19 40-29.5t49-10.5q26 0 49 10.5t40 29.5h167l84-360H182l4-17q6-28 27.5-45.5T264-800h456l-37 160h117l120 160-40 200h-80q0 50-35 85t-85 35q-50 0-85-35t-35-85H400q0 50-35 85t-85 35Zm357-280h193l4-21-74-99h-95l-28 120Zm-19-273 2-7-84 360 2-7 34-146 46-200ZM20-427l20-80h220l-20 80H20Zm80-146 20-80h260l-20 80H100Zm180 333q17 0 28.5-11.5T320-280q0-17-11.5-28.5T280-320q-17 0-28.5 11.5T240-280q0 17 11.5 28.5T280-240Zm400 0q17 0 28.5-11.5T720-280q0-17-11.5-28.5T680-320q-17 0-28.5 11.5T640-280q0 17 11.5 28.5T680-240Z";
+      } else if (item.service_type == "shopping") {
+        svg_path =
+          "M240-80q-33 0-56.5-23.5T160-160v-480q0-33 23.5-56.5T240-720h80q0-66 47-113t113-47q66 0 113 47t47 113h80q33 0 56.5 23.5T800-640v480q0 33-23.5 56.5T720-80H240Zm0-80h480v-480h-80v80q0 17-11.5 28.5T600-520q-17 0-28.5-11.5T560-560v-80H400v80q0 17-11.5 28.5T360-520q-17 0-28.5-11.5T320-560v-80h-80v480Zm160-560h160q0-33-23.5-56.5T480-800q-33 0-56.5 23.5T400-720ZM240-160v-480 480Z";
+      } else if (item.service_type == "errands") {
+        svg_path =
+          "M520-40v-240l-84-80-40 176-276-56 16-80 192 40 64-324-72 28v136h-80v-188l158-68q35-15 51.5-19.5T480-720q21 0 39 11t29 29l40 64q26 42 70.5 69T760-520v80q-66 0-123.5-27.5T540-540l-24 120 84 80v300h-80Zm20-700q-33 0-56.5-23.5T460-820q0-33 23.5-56.5T540-900q33 0 56.5 23.5T620-820q0 33-23.5 56.5T540-740Z";
+      }
+      html += `
+      
+            <div class="service-request" id="${item.id}">
+                <div class="top">
+                    <svg class="service-icon" viewBox="0 -960 960 960">
+                        <path d="${svg_path}" />
+                    </svg> 
+                    <h3>${item.service_type}</h3>
+                </div>
+                <div class="bottom">
+                    <p class="time">3:30pm</p>
+                     <a href="request-details.html?id=${item.id}" class="details-button">Details</a>
+                </div>
+            </div>
+     
+
+        `;
+    });
+    main.innerHTML = html;
+  });
---
+++ request-details.js
@@ -0,0 +1,93 @@
+const requestDetailCard = document.querySelector(".request-detail-card-main");
+const acceptButton = document.querySelector(".accept-button");
+let html = "";
+const backend_url =
+  "https://errand-app-backend.onrender.com/api/service_requests/";
+const completedButton = document.querySelector(".completed-button");
+
+params = new URLSearchParams(window.location.search);
+const id = params.get("id");
+
+fetch(`${backend_url}/${id}`)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    html = `
+    <h2>${data.service_type} Request</h2>
+      <p class="status"></p>
+      <p><span class="blue">Customer Name: </span>${data.username}</p>
+      <p><span class="blue">Email: </span>${data.user_email}</p>
+      <p><span class="blue">Phone: </span>${data.user_phone}</p>
+       <p> <span class="blue">Details: </span>${data.request_details}</p>
+      <p><span class="blue">Additional Info: </span>John Doe</p>
+    `;
+    if (data.service_type === "delivery") {
+      html += `
+        <p><span class="blue">Pickup Location: </span>John Doe</p>
+      <p><span class="blue">Drop off Location: </span>John Doe</p>
+      
+        `;
+    } else if (data.service_type === "errands") {
+      html += `<p><span class="blue">Errand Destination: </span>${data.errand_destination}</p>
+      <p><span class="blue">Delivery Location: </span>${data.delivery_location}</p>`;
+    } else if (data.service_type === "shopping") {
+      html += ` <p><span class="blue">Delivery Location: </span>${data.delivery_location}</p>`;
+    }
+    requestDetailCard.innerHTML = html;
+
+    if (data.status === "pending") {
+      acceptButton.style.display = "block";
+      document.querySelector(".status").innerText = "Pending";
+      document.querySelector(".status").style.color = "red";
+    } else if (data.status === "accepted") {
+      acceptButton.style.display = "none";
+      document.querySelector(".status").innerText = "Accepted";
+      document.querySelector(".status").style.color = "green";
+      completedButton.style.display = "block";
+    }
+    if (data.completed) {
+      acceptButton.style.display = "none";
+      completedButton.style.display = "none";
+      document.querySelector(".completed").style.display = "block";
+    }
+  });
+
+function acceptRequest(id) {
+  fetch(`${backend_url}/${id}/`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      status: "accepted",
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Updated:", data);
+      alert("Request accepted!");
+      document.querySelector(".status").innerText = "Accepted";
+      window.location.reload();
+    });
+}
+
+function markAsCompleted(id) {
+  fetch(`${backend_url}/${id}/`, {
+    method: "PATCH",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ completed: "True" }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("updated:", data);
+    });
+}
+
+acceptButton.addEventListener("click", () => {
+  acceptRequest(id);
+});
+completedButton.addEventListener("click", () => {
+  markAsCompleted(id);
+  document.querySelector(".completed").style.display = "block";
+  completedButton.style.display = "none";
+});
